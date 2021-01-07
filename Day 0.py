26ff5c1eb2e18fc21b8cb099338398d8b1f7e072
Akash Choudhary <74610406+Akashcba@users.noreply.github.com>
2021-01-07 21:40:40 +0530

    Day 0
    First File

---
+++ Day 0.py
@@ -0,0 +1,10 @@
+# Basic Input Output
+# Take input from stdin
+# Store to a variable
+# print hello, World.
+# print value_of_the_variable
+
+input_str = input()
+
+print("Hello, World.")
+print(input_str,sep='')

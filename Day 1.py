094e6d143e5bb994799c29e092692eeb13f4675b
Akash Choudhary <74610406+Akashcba@users.noreply.github.com>
2021-01-08 17:19:20 +0530

    Day 1 --Final Commit

---
+++ Day 1.py
@@ -0,0 +1,20 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Tasks :
+# Declare 3 variables - One of type int, double and string.
+# Read 3 lines of input from stdin and initialise the variables.
+# Print operation on the variables .
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+ls = []
+ls.append(int(input()) )
+ls.append(float(input()) )
+ls.append(input() )
+
+print(i + ls[0])
+print(d + ls[1])
+print(s + ls[2])
